1ade956b0455faa61a5fef8f71396db78419072d
unknown <niccolo.panciroli@gmail.com>
2020-12-01 15:36:39 +0100

    aggiungo il file lancio.py

---
+++ lancio.py
@@ -0,0 +1,13 @@
+partecipanti = []
+lanci= []
+numero = int(input("Inserisci il numero dei partecipanti alla gara sportiva "))
+contatore = 0
+while contatore != numero: 
+    contatore= contatore + 1
+    nome = str(input("Inserisci il nome dello studente "))
+    lancio= int(input("Inserisci la distanza del lancio dello studente "))
+    partecipanti.append(nome)
+    lanci.append(lancio)
+risultato = zip(partecipanti, lanci)
+print("I nomi dei pertecipanti con i relativi risultati: ",list(risultato))
+print("Il lancio massimo è ", max(lanci))
